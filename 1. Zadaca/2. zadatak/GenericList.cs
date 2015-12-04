922852f26ba451afb2bc7ea4f37939b64ad45737
Sebastian Dovičin <sebastian.dovicin@fer.hr>
2015-12-04 19:57:02 +0100

    zadatak 2. kompletno rijesen

---
+++ 1. Zadaca/2. zadatak/GenericList.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.zadatak
+{
+    class GenericList<X> : IGenericList<X>
+    {
+
+        private X[] _internalStorage;
+        private int _storageElementsCount = 0;
+
+        //Constructors
+        public GenericList()
+        {
+            _internalStorage = new X[4];
+            
+        }
+
+        public GenericList(int InitialSize)
+        {
+            _internalStorage = new X[InitialSize];
+        }
+
+        //IGenericList methods
+        public void Add(X item)
+        {
+            if (_internalStorage.Length == _storageElementsCount)
+            {
+                X[] TemporaryStorage = new X[_internalStorage.Length * 2];
+                _internalStorage.CopyTo(TemporaryStorage, 0);
+                _internalStorage = TemporaryStorage;
+            }
+
+            _internalStorage[_storageElementsCount] = item;
+            ++_storageElementsCount;
+        }
+
+        public bool Remove(X item)
+        {
+            int StoragePosition = IndexOf(item);
+            if (StoragePosition == -1)
+                return false;
+            else
+                return RemoveAt(StoragePosition);
+        }
+
+        public bool RemoveAt(int index)
+        {
+            if (index > (_internalStorage.Length - 1))
+                return false;
+
+            if (_internalStorage[index].Equals(default(X))) return false;
+            else
+            {
+                _internalStorage[index] = default(X); //je li ovo valjano?
+                for (int i = index + 1; i < _internalStorage.Length; ++i)
+                    _internalStorage[i - 1] = _internalStorage[i];
+                --_storageElementsCount;
+                return true;
+            }
+        }
+
+        public X GetElement(int index)
+        {
+            if (index <= _internalStorage.Length)
+                return _internalStorage[index];
+            else
+                throw new IndexOutOfRangeException();
+        }
+
+        public int IndexOf(X item)
+        {
+            for (int i = 0; i < _internalStorage.Length; ++i)
+            {
+                if (_internalStorage[i].Equals(item))
+                    return i;
+            }
+            return -1;
+        }
+
+        public int Count
+        {
+            get 
+            {
+                return _storageElementsCount;
+            }
+        }
+
+        public void Clear()
+        {
+            _internalStorage = new X[_internalStorage.Length];
+            _storageElementsCount = 0;
+        }
+
+        public bool Contains(X item)
+        {
+            for (int i = 0; i < _internalStorage.Length; ++i)
+                if (_internalStorage[i].Equals(item))
+                    return true;
+            return false;
+        }
+    }
+}
---
+++ 1. Zadaca/2. zadatak/IGenericList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.zadatak
+{
+    interface IGenericList<X>
+    {
+        // Adds an item to the collection
+        void Add(X item);
+
+        // Removes the first occurrence of an item from the collection.
+        // If the item was not found, method does nothing.
+        bool Remove(X item);
+
+        // Removes the item at the given index in the collection.
+        bool RemoveAt(int index);
+
+        // Returns the item at the given index in the collection.
+        X GetElement(int index);
+
+        // Returns the index of the item in the collection.
+        // If item is not found in the collection, method returns -1.
+        int IndexOf(X item);
+
+        // Readonly property. Gets the number of items contained in the collection.
+        int Count { get; }
+
+        // Removes all items from the collection.
+        void Clear();
+
+        //Determines whether the collection contains a specific value.
+        bool Contains(X item);
+
+    }
+}
